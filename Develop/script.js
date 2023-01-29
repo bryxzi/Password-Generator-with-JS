f0b74357d70d8390b1b3a87eb4d4df00f91440aa
bryxzi <122295924+bryxzi@users.noreply.github.com>
2023-01-29 02:31:28 -0500

    added validator

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,33 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  var passwordText = document.querySelector('#password');
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
+
+
+
+function generatePassword () {
+
+  let passlength = window.prompt('how long do you want your password to be?')
+  console.log(passlength);
+
+  let validator;
+
+  if (passlength >= 8 && passlength <= 128)
+  validator = true;
+else {
+  console.log('invalid password length');
+  return;
+}
+
+
+}
+
+
