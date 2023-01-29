2be92b139a5cd1c3f81b533be3cd9f0e24157cbe
bryxzi <122295924+bryxzi@users.noreply.github.com>
2023-01-29 12:46:35 -0500

    added strict validators, displayed generated  password onto html

--- Develop/script.js
+++ Develop/script.js
@@ -1,53 +1,44 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
   passwordText.value = password;
+  
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
-
-
-
-function generatePassword () {
-
-  let passlength = window.prompt('how long do you want your password to be?')
-  console.log(passlength);
-
-  let validator;
-
-  if (passlength >= 8 && passlength <= 128)
-  validator = true;
-else {
-  console.log('invalid password length');
-  return;
-}
-
-let lowercase = window.confirm('do you want lowercase letters?');
-let uppercase = window.confirm('do you want uppercase letters?');
-let numeric = window.confirm('do you want numbers?');
-let special = window.confirm('do you want special characters?');
-
-if (!lowercase && !uppercase && !numeric && !special) {
-console.log('you must atleast select one');
-return;
-}
-
-var password = "";
-var characters = "";
-if (lowercase) characters += "abcdefghijklmnopqrstuvwxyz";
-if (uppercase) characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-if (numeric) characters += "0123456789";
-if (special) characters += "!@#$%^&*()_+-=[]{}|;':\"<>,.?/\\";
-
-for (let i = 0; i < passlength; i++) 
+function generatePassword() {
+  let passlength = window.prompt('How long do you want your password to be?')
+  
+  if (passlength < 8 || passlength > 128 || isNaN(passlength)) {
+    window.alert('Invalid password length, please try again!');
+    return;
+  }
+  
+  let lowercase = window.confirm('Do you want lowercase letters?');
+  let uppercase = window.confirm('Do you want uppercase letters?');
+  let numeric = window.confirm('Do you want numbers?');
+  let special = window.confirm('Do you want special characters?');
+  
+  if (!lowercase && !uppercase && !numeric && !special) {
+    console.log('You must choose one, please try again!');
+    return;
+  }
+  
+  var password = "";
+  var characters = "";
+  if (lowercase) characters += "abcdefghijklmnopqrstuvwxyz";
+  if (uppercase) characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (numeric) characters += "0123456789";
+  if (special) characters += "!@#$%^&*()_+-=[]{}|;':\"<>,.?/\\";
+  
+  for (let i = 0; i < passlength; i++)
   password += characters[Math.floor(Math.random() * characters.length)];
   
-  console.log(password);
+  return password;
 }
+
+generateBtn.addEventListener('click', writePassword);
 
 
